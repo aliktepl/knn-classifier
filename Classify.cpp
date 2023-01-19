3202109ee0f574ec914efae0a4c8b89e372f9033
aliktepl <aliktepl@gmail.com>
2023-01-19 10:43:06 +0200

    added read and write functionality to each command.

--- Classify.cpp
+++ Classify.cpp
@@ -3,11 +3,20 @@
 //
 
 #include "Classify.h"
+#include "kNearestNeighbors.h"
 
 Classify::Classify(DefaultIO *dio) : Command(dio) {
     this->description = "3. classify data";
 }
 
 void Classify::execute() {
-
+    //default p value for the minkowski distance;
+    int p = 2;
+    vector<Classified> classifiedVectors;
+    string distance;
+    vector<double> input;
+    int k = 5;
+    kNearestNeighbors knn(classifiedVectors, input, k);
+    string cat = knn.mapDistances(knn.calculateDistances(distance, p));
+    dio->write("classifying data complete.");
 }
--- Settings.cpp
+++ Settings.cpp
@@ -9,5 +9,9 @@ Settings::Settings(DefaultIO *dio) : Command(dio) {
 }
 
 void Settings::execute() {
-
+    //default values
+    int k = 5;
+    string metric = "EUC";
+    string settings_input = dio->read();
+    dio->write("The current KKN parameters are: K = " + to_string(k) + ", distance metric = " + metric);
 }
